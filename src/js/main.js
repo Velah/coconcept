90ad44a407650ebbb5867a11a3ff1c3cef06e9c8
Xavier Velasco Llauradó <velasco.llaurado@gmail.com>
2019-01-31 22:40:44 +0100

    Retoques

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 $('.navbar-brand a').on("click", function() {
+    $('nav.navbar:not(has-shadow)').addClass("has-shadow");
     $('.hidden-section').hide();
     $('.main-section').show();
     $('nav').show();
@@ -6,12 +7,14 @@ $('.navbar-brand a').on("click", function() {
 });
 
 $('#openContactButton').on("click", function () {
+    $('nav.navbar:not(has-shadow)').addClass("has-shadow");
     $('#contactSection').show();
     $('nav').hide();
     $('.main-section').hide();
 });
 
 $('#closeContactButton').on("click", function () {
+    $('nav.navbar:not(has-shadow)').addClass("has-shadow");
     $('#contactSection').hide();
     $('nav').show();
     $('#landingSection').show();
@@ -20,6 +23,7 @@ $('#closeContactButton').on("click", function () {
 });
 
 $('#knowMeButton').on("click", function() {
+    $('nav.navbar:not(has-shadow)').addClass("has-shadow");
     $('#dialogSection').show();
     $('.navbar-menu').hide();
     $('#contactSection').hide();
@@ -29,6 +33,7 @@ $('#knowMeButton').on("click", function() {
 });
 
 $('.portfolioImg').on("click", function() {
+    $('nav.navbar:not(has-shadow)').addClass("has-shadow");
     $($(this).attr("targetSection")).show();
     $('.navbar-menu').hide();
     $('#contactSection').hide();
@@ -38,18 +43,21 @@ $('.portfolioImg').on("click", function() {
 });
 
 $("[href='#workflowSection']").click(function() {
+    $('nav.navbar:not(has-shadow)').addClass("has-shadow");
     $('html, body').animate({
-        scrollTop: $("#workflowSection").offset().top
+        scrollTop: $("#landingSection")[0].scrollHeight - 20
     }, 2000, 'easeOutQuart');
 });
 
 $("[href='#portfolioSection']").click(function() {
+    $('nav.navbar:not(has-shadow)').addClass("has-shadow");
     $('html, body').animate({
         scrollTop: $("#portfolioSection").offset().top
     }, 2000, 'easeOutQuart');
 });
 
 $("[href='#landingSection']").click(function() {
+    $('nav.navbar.has-shadow').removeClass("has-shadow");
     $('html, body').animate({
         scrollTop: 0
     }, 2000, 'easeOutQuart');
